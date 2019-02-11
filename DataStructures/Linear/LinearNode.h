6078c19fbde1950045e4176676ae126eaf3604cf
Kaylie Hollander <Kphollander1@gmail.com>
2019-02-11 09:34:28 -0700

    Added Queue, LinkedList,  and LinearNode files

---
+++ DataStructures/Linear/LinearNode.h
@@ -0,0 +1,13 @@
+//
+//  LinearNode.h
+//  DataStructures
+//
+//  Created by Hollander, Kaylie on 2/7/19.
+//  Copyright © 2019 Hollander, Kaylie. All rights reserved.
+//
+
+#ifndef LinearNode_h
+#define LinearNode_h
+
+
+#endif /* LinearNode_h */
---
+++ DataStructures/Linear/LinkedList.hpp
@@ -0,0 +1,156 @@
+//
+//  LinkedList.hpp
+//  DataStructures
+//
+//  Created by Hollander, Kaylie on 2/7/19.
+//  Copyright © 2019 Hollander, Kaylie. All rights reserved.
+//
+
+#ifndef LinkedList_hpp
+#define LinkedList_hpp
+
+#include "List.hpp"
+
+using namespace std;
+
+template <class Type>
+class LinkedList : public List<Type>
+{
+public:
+//    Constructors
+    LinkedList();
+//    Destructor
+    virtual ~LinkedList();
+//    Helper Methods
+    int getsize() const;
+    LinearNode<Type> * getFront();
+    LinearNode<Type> * getEnd();
+    
+//    Structure Methods
+    void add(Type item);
+    void addAtIndex(int index, Type item);
+    Type getFromIndex(int index);
+    Type remove(int index);
+    bool contains(Type item);
+protected:
+    LinearNode<Type> * front;
+    LinearNode<Type * end;
+};
+    
+template <class Type>
+LinkedList<Type> :: LinkedList()
+{
+    this -> front = nullptr;
+    this -> end = nullptr;
+    this -> size = 0;
+}
+    
+template <class Type>
+LinkedList<Type> :: ~LinkedList()
+{
+    LinearNode<Type> * destroyStructure = front;
+    while (front != nullptr)
+    {
+        front = destroyStructure -> getNextNode();
+        delete destroyStructure;
+        destroyStructure = front;
+    }
+}
+    
+template <class Type>
+void LinkedList<Type> :: addAtIndex(int index, Type Item)
+{
+    assert(index >+ 0 && index <= this -> size);
+    if(index == this -> size)
+    {
+        add(item);
+    }
+    else
+    {
+        LinearNode<Type> * toBeAdded = new LinearNode<Type>(item);
+        if (index == 0)
+        {
+            toBeAdded -> setNextNode(front);
+            front = toBeAdded;
+        }
+        else
+        {
+            LinearNode<Type> * previous = nullptr;
+            LinearNode<Type> * current = front;
+            
+            for(int position = 0; position < index; position++)
+            {
+                previous = current;
+                current = current -> getNextNode();
+            }
+            previous -> setNextNode(toBeAdded);
+            toBeAdded -> setNextNode(current);
+        }
+        this -> size++;
+    }
+}
+    
+template <class Type>
+Type LinkedList<Type> :: remove(int index)
+{
+    assert(index >= 0 && index < this -> size);
+    
+    LinearNode<Type> * current = front;
+    LinearNode<Type> * toBeRemoved = nullptr;
+    LinearNode<Type> * previous = nullptr;
+    
+    Type removedData;
+    
+    if (index == 0)
+    {
+        toBeRemoved = front;
+        this -> front = this -> front -> getNextNode();
+    }
+    else
+    {
+        for(int position = 0; position < index; position++)
+        {
+            previous = current;
+            current = current -> getNextNode();
+        }
+        
+        toBeRemoved = current;
+        
+        if(index == this -> size - 1)
+        {
+            previous -> setNextNode(nullptr);
+            end = previous;
+        }
+        else
+        {
+            current = toBeRemoved -> getNextNode();
+            previous -> setNextNode(current);
+        }
+    }
+    
+    this -> size -= 1;
+    
+    removedData = toBeRemoved -> getData();
+    delete toBeRemoved;
+    return removedData;
+}
+    
+template <class Type>
+    bool LinkedList<Type> :: contains(Type thingToFind)
+    {
+        bool exists = false;
+        
+        LinearNode<Type> * searchPointer = front;
+        
+        for(int index = 0; index < getSize(); index++)
+        {
+            if(searchPointer -> getData() == thingToFind)
+            {
+                return true;
+            }
+            searchPointer = searchPointer -> getNextNode();
+        }
+        
+        return exists;
+    }
+#endif /* LinkedList_hpp */
